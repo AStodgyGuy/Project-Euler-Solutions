a852b57add2b4ce996556663a003fbe716a04be9
Justin Yang <Justin Yang>
2016-12-18 23:34:18 +0000

    Solution to problem 10

---
+++ Problem 10/Problem10.java
@@ -0,0 +1,25 @@
+public class Problem10 {
+    public static void main(String[] args) {
+        long sum = 2;
+        double n;
+
+        for (int i = 3; i < 2000000; i++) {
+            n = Math.sqrt((double)i);
+            if(isPrime(n, i)){
+                sum += i;
+            }
+        }
+        System.out.println(sum); 
+    }
+
+    public static boolean isPrime(double n, int i) {
+        for (int j = 2; j <= n; j++) {
+            if (i % j == 0){ 
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
